41cd929dce0a55b8568f7e85ebf364e3aab4970d
KrashLeviathan <nathankarasch@gmail.com>
2016-07-10 16:37:54 -0500

    Added _tools/tidy_all_html.sh and added javascript type to all html head elements

--- _tools/tidy_all_html.sh
+++ _tools/tidy_all_html.sh
@@ -6,15 +6,37 @@
 # Current directory
 CDIR=$(pwd)
 
+# Colors for output
+RED='\033[0;31m'
+YEL='\033[1;33m'
+NC='\033[0m' # No Color
+
 # Iterate through the directories recursively
 for i in $(ls -R | grep :); do
     DIR=${i%:}                    # Strip ':'
-    echo "cd $DIR"
     cd ${DIR}
-    # Iterate through the html files in the directory
-    for FILENAME in ./*.html; do
-        # Tidy up the file (make it formatted pretty)
-        tidy -i -m -q -w 120 -ashtml -utf8 "$FILENAME"
-    done
+
+    ISEXCLUDED=`echo "$DIR" | grep "_posts" | wc -l`
+    if [ $ISEXCLUDED != 0 ]
+    then
+        printf "${YEL}Excluding the _posts/ directory${NC}\n"
+        cd $CDIR
+        continue
+    fi
+
+    # If html files exist in the directory
+    COUNT=`ls -1 *.html 2>/dev/null | wc -l`
+    if [ $COUNT != 0 ]
+    then
+        printf "$DIR"
+        # Iterate through the html files in the directory
+        for FILENAME in ./*.html; do
+            printf "    $FILENAME ${RED}\n"
+            # Tidy up the file (make it formatted pretty)
+            tidy -i -m -q -w 120 -ashtml -utf8 "$FILENAME"
+            printf "$NC"
+        done
+    fi
+
     cd $CDIR
 done
